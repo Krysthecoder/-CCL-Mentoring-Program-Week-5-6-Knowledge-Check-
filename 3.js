d399ca5eca1ab35058885ce533588d2cd178d78d
The Krys <krysthopher@gmail.com>
2024-05-22 19:52:41 -0600

    added third challenge

---
+++ 3.js
@@ -0,0 +1,4 @@
+//  Title: Convert boolean values to strings 'Yes' or 'No'.
+//  Link:  https://www.codewars.com/kata/53369039d7ab3ac506000467/solutions/javascript?filter=me&sort=best_practice&invalids=false
+
+const boolToWord = bool => bool === true ? 'Yes' : 'No'
