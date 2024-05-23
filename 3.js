7175c3d1d8868087c7b27abd92efe8e6d34791e2
The Krys <krysthopher@gmail.com>
2024-05-22 19:57:04 -0600

    added new TS version to challenge 3

--- 3.js
+++ 3.js
@@ -2,3 +2,9 @@
 //  Link:  https://www.codewars.com/kata/53369039d7ab3ac506000467/solutions/javascript?filter=me&sort=best_practice&invalids=false
 
 const boolToWord = bool => bool === true ? 'Yes' : 'No'
+
+//  TypeScrips works the same, so there is no worth on creating a new file for it
+
+export const boolToWord2 = (bool: boolean): string => {
+    return bool ? 'Yes' : 'No';
+};
